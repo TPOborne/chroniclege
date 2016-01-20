f8b1f63c6d4b79171d1aa9b0007217bb21dc8fd1
Tom Oborne <tpoborne@gmail.com>
2016-01-20 15:47:52 +0000

    edit card

--- edit_cards.php
+++ edit_cards.php
@@ -6,7 +6,6 @@ if(isset($_POST['submit'])){
     $cardId = $_POST['cardId'];
     $attack = $_POST['attack'];
     $health = $_POST['health'];
-    echo $attack.$health." ".$cardId;
     mysql_query("UPDATE cards SET attack='$attack', health='$health' WHERE id='$cardId'");
 }
 ?>
@@ -16,13 +15,26 @@ if(isset($_POST['submit'])){
         <title>ChronicleGE</title>
         <link rel='stylesheet' href='css/reset.css' type='text/css'/>
         <link rel='stylesheet' href='css/main.css' type='text/css'/>
+        <link rel='stylesheet' href='css/header.css' type='text/css'/>
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Crimson+Text' type='text/css'/>
         <script src="lib/jquery-1.11.3.js"></script>
         <script src="lib/jquery-ui.min.js"></script>
         <script src="lib/jquery.ui.touch-punch.min.js"></script>
+        <style>
+            input[type='text'] {
+                width: 40px;
+                padding: 10px;
+                font-size: 20px;
+            }
+            form {
+                float: left;
+                width: 100%;
+            }
+        </style>
     </head>
     <body>
         <div class="mainContainer">
+           <?php include_once("includes/header.php"); ?>
             <div class="backgroundContainer">
                 <h2>Let's Start</h2>
                 <?php
@@ -34,10 +46,11 @@ if(isset($_POST['submit'])){
                 ?>
                 <form action="" method="POST">
                     <input type="hidden" name="cardId" value="<?php echo $cardId ?>"/>
-                    <input type="text" name="attack"/>
-                    <input type="text" name="health"/>
+                    Attack <input type="text" name="attack"/>
+                    Health <input type="text" name="health"/>
                     <input type="submit" value="sumbit" name="submit"/>
                 </form>
+                <div class="clear"></div>
             </div>
     </body>
 </html>
